d706bd3c19166b70a59d11281ac3e79f7dc92d24
Aiste Laugayte <aiste.laugalyte@gmail.com>
2020-04-20 15:32:50 +0300

    Changed error page

--- templates/main/pages/error.php
+++ templates/main/pages/error.php
@@ -1,3 +1,12 @@
-<h2 class="mt-4">OOOOOOOOOOOOOOOOOPS!!!!</h2>
-
-<a class="btn btn-success mb-3" href="?p=home" role="button"><i class="fas fa-home"></i> Pagrindinis</a>
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-sm-12 col-md">
+            <img src="templates/main/img/monster.png" class="img-fluid" alt="cookie monster">
+        </div>
+        <div class="col-sm-12 col-md text-center">
+            <h2 class="mt-4">UPS! Čia nieko nėra.</h2>
+            <h3>Ne, sausainių aš nemačiau!</h3>
+            <a class="btn btn-success my-2" href="?p=home" role="button">Grįžti į pagrindinį</a>
+        </div>
+    </div>
+</div>
