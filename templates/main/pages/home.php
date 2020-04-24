71aed34af42b0c4c74c4be143a0fd269c65fce5a
Aiste Laugayte <aiste.laugalyte@gmail.com>
2020-04-24 12:04:08 +0300

    Added some content to home page

--- templates/main/pages/home.php
+++ templates/main/pages/home.php
@@ -1,3 +1,8 @@
-<h1 class="mt-4">Simple Sidebar</h1>
-<p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
-<p>Make sure to keep all page content within the <code>#page-content-wrapper</code>. The top navbar is optional, and just for demonstration. Just create an element with the <code>#menu-toggle</code> ID which will toggle the menu when clicked.</p>
+<div class="alert alert-success mt-4" role="alert">
+    <h2 class="alert-heading">Filmų duomenų bazė</h2>
+    <hr>
+    <h5>Mokomasis PHP projektas su duomenų baze</h5>
+    <p>Pagrindinis projekti tikslas, sukurti aplikacijos struktūrą, navigaciją, puslapius(pagal paskirtį su formomis ir lentelėmis informacijos rodymui), bei router'į, kuris apdoroja vartotojo užklausas, kai paspaudžiama nuoroda navigacijoje.</p>
+    <hr>
+    <p class="mb-0">Projektas atliktas mokymosi tikslais.</p>
+</div>
